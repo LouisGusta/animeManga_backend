7738254471384cff391d48b0daa3b5f610a638ef
Raul Luca <raulgalatafc@gmail.com.br>
2021-11-01 10:58:20 -0300

    Reajuste + Pasta anime

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const routes = express.Router()
 
-const searchManga = require('../src/controllers/searchManga')
-const returnMangaInfo = require('../src/controllers/returnMangaInfo')
+// Define routes - Manga
+const searchManga = require('../src/controllers/manga/searchManga')
+const returnManga = require('../src/controllers/manga/returnManga')
 
-routes.get('/searchManga', searchManga.indexManga)
-routes.get('/manga/resume', returnMangaInfo.returnInfo)
-routes.get('/manga/characters', returnMangaInfo.returnCharacter)
+routes.get('/manga/searchManga', searchManga.indexManga)
+routes.get('/manga/resume', returnManga.returnInfo)
+routes.get('/manga/characters', returnManga.returnCharacter)
+
+// Define routes - Anime
+
+const searchAnime = require('../src/controllers/anime/searchAnime')
+//const returnAnime = require('../src/controllers/anime/returnAnime')
+
+routes.get('/anime/searchAnime', searchAnime.indexAnime)
 
 module.exports = routes
---
+++ src/controllers/anime/searchAnime.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const axios = require('axios')
+
+module.exports = {
+    async indexAnime(req, res){
+        res.json({'status': 200})
+    }
+}
