e6d9530480cc4b1ad830d5b8ef1c7731dd5ae5e3
Raul Luca <raulgalatafc@gmail.com.br>
2021-11-01 16:25:19 -0300

    Ajustado infra

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const routes = express.Router()
 
-// Define routes - Manga
-const searchManga = require('../src/controllers/manga/searchManga')
-const returnManga = require('../src/controllers/manga/returnManga')
+// Define route test
+routes.get('/', (req, res) => res.json({"status": "API Running"}))
 
-routes.get('/manga/searchManga', searchManga.indexManga)
+// Define routes - Manga
+const searchManga = require('../src/controllers/manga/search')
+const returnManga = require('../src/controllers/manga/resume')
+
+routes.get('/manga/search', searchManga.indexManga)
 routes.get('/manga/resume', returnManga.returnInfo)
 routes.get('/manga/characters', returnManga.returnCharacter)
 
 // Define routes - Anime
 
-const searchAnime = require('../src/controllers/anime/searchAnime')
+const searchAnime = require('../src/controllers/anime/search')
 //const returnAnime = require('../src/controllers/anime/returnAnime')
 
-routes.get('/anime/searchAnime', searchAnime.indexAnime)
+routes.get('/anime/search', searchAnime.indexAnime)
 
 module.exports = routes
---
+++ src/controllers/manga/search.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const axios = require('axios')
+
+module.exports = {
+    async indexManga(req, res) {
+        const mangaList = []
+        var itemsProcessed = 0;
+
+        graphQuery = `
+        query ($page: Int, $perPage: Int, $name: String) {
+            Page (page: $page, perPage: $perPage) {
+                pageInfo {
+                    total
+                    currentPage
+                    lastPage
+                    hasNextPage
+                    perPage
+                }
+            media (sort:SCORE_DESC, search: $name, format:MANGA) {
+                id
+                status
+                format
+                bannerImage
+                coverImage{
+                    extraLarge
+                }
+                genres
+                isAdult 
+                averageScore
+                title {
+                    romaji
+                    english
+                    native
+                }
+            }
+        }
+        }
+        `
+        await axios({
+            url: 'https://graphql.anilist.co',
+            method: "POST",
+            data: {
+                query: graphQuery,
+                variables: {
+                    name: req.headers.querysearch
+                }
+            }
+        }).then((ok) => {
+            console.log(ok.data.data.Page.media)
+            res.json(ok.data.data.Page.media)
+        })
+    },
+}
+
